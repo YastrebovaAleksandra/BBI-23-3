53e53c2beb1500a68db6436a8bcc773f4bdd6383
YastrebovaAleksandra <159530005+YastrebovaAleksandra@users.noreply.github.com>
2024-05-29 22:55:16 +0300

    9.2, 9.3

---
+++ 9.2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class Serializer
+{
+    public void SerializeToJson(object obj, string filePath)
+    {
+        string json = JsonConvert.SerializeObject(obj);
+        File.WriteAllText(filePath, json);
+    }
+
+    public T DeserializeFromJson<T>(string filePath)
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
+
+class Human
+{
+    protected string _name;
+    protected int _wincount;
+    protected double _drawcount;
+    protected int _loosecount;
+    protected double _finalscore;
+
+    public Human(string name, int wins, double draws, int looses)
+    {
+        _name = name;
+        _wincount = wins;
+        _drawcount = draws;
+        _loosecount = looses;
+        _finalscore = wins * 1 + draws / 2;
+    }
+
+    public double Finalscore => _finalscore;
+
+    public virtual void Print()
+    {
+        Console.WriteLine("{0,-5} | {1,-3} | {2,-3} | {3,-7} | {4,-3}", _name, _wincount, _drawcount, _loosecount, _finalscore);
+    }
+}
+
+class Athlete : Human
+{
+    static int _id = 0;
+    private int ID;
+
+    public Athlete(string name, int wins, double draws, int looses) : base(name, wins, draws, looses)
+    {
+        _id++;
+        ID = _id;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("{5,-3} | {0,-5} | {1,-3} | {2,-3} | {3,-7} | {4,-3}", _name, _wincount, _drawcount, _loosecount, _finalscore, ID);
+    }
+}
+
+class Program
+
+{
+    static void EnsureDirectoryExists(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+    static void Main(string[] args)
+    {
+        string directoryPath = @"C:\Users\Yastr\OneDrive\Рабочий стол\Учёба\программирование\лаб9";
+        EnsureDirectoryExists(directoryPath);
+        Athlete[] participants = new Athlete[7]
+        {
+            new Athlete("Vlad", 1, 2, 7),
+            new Athlete("Pavel", 4, 3, 2),
+            new Athlete("Lera", 0, 2, 9),
+            new Athlete("Diana", 2, 8, 1),
+            new Athlete("Vera", 3, 3, 7),
+            new Athlete("Denis", 3, 6, 3),
+            new Athlete("Anton", 4, 6, 3)
+        };
+
+        Console.WriteLine("Список участников");
+        Console.WriteLine("Имя   Победы Ничьи Поражения Результат");
+        for (int i = 0; i < participants.Length; i++)
+        {
+            participants[i].Print();
+        }
+
+        
+        Serializer serializer = new Serializer();
+        serializer.SerializeToJson(participants, @"C:\Users\Yastr\OneDrive\Рабочий стол\Учёба\программирование\лаб9");
+
+        
+        Athlete[] loadedParticipants = serializer.DeserializeFromJson<Athlete[]>(@"C:\Users\Yastr\OneDrive\Рабочий стол\Учёба\программирование\лаб9");
+
+        
+        Console.WriteLine("\nЗагруженные участники");
+        Console.WriteLine("Имя   Победы Ничьи Поражения Результат");
+        for (int i = 0; i < loadedParticipants.Length; i++)
+        {
+            loadedParticipants[i].Print();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ 9.3.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class JSONSerializer
+{
+    public void Serialize<T>(T obj, string filePath)
+    {
+        string json = JsonConvert.SerializeObject(obj);
+        File.WriteAllText(filePath, json);
+    }
+
+    public T Deserialize<T>(string filePath)
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
+
+public class MainProgram
+{
+    static void Main(string[] args)
+    {
+        string directoryPath = @"C:\Users\Yastr\OneDrive\Рабочий стол\Учёба\программирование\лаб9";
+
+        string filePathA = Path.Combine(directoryPath, "dataA.json");
+        Student[] studentsA = new Student[]
+        {
+            new Student("Mari", "Ivanova", new int[] { 87, 77, 65, 98 }),
+            new Student("Misha", "Smirnov", new int[] { 89, 88, 76, 66 }),
+        };
+        JSONSerializer serializer = new JSONSerializer();
+        serializer.Serialize(studentsA, filePathA);
+        Student[] loadedStudentsA = serializer.Deserialize<Student[]>(filePathA);
+
+        foreach (var student in loadedStudentsA)
+        {
+            student.Print();
+        }
+
+
+        string filePathB = Path.Combine(directoryPath, "dataB.json");
+        Student[] studentsB = new Student[]
+        {
+            new Student("Anton", "Volkov", new int[] { 67, 65, 87, 93 }),
+            new Student("Dasha", "Mishina", new int[] { 68, 59, 67, 88 }),
+        };
+        serializer.Serialize(studentsB, filePathB);
+        Student[] loadedStudentsB = serializer.Deserialize<Student[]>(filePathB);
+
+        foreach (var student in loadedStudentsB)
+        {
+            student.Print();
+        }
+
+        string filePathC = Path.Combine(directoryPath, "dataC.json");
+        Student[] studentsC = new Student[]
+        {
+            new Student("Kate", "Savina", new int[] { 80, 79, 62, 59 }),
+            new Student("Alex", "Lobov", new int[] { 65, 73, 84, 95 }),
+        };
+        serializer.Serialize(studentsC, filePathC);
+        Student[] loadedStudentsC = serializer.Deserialize<Student[]>(filePathC);
+
+        foreach (var student in loadedStudentsC)
+        {
+            student.Print();
+        }
+
+        foreach (var student in studentsA)
+        {
+            Console.WriteLine($"Before serialization - {student.FirstName} {student.LastName}: Exams - {string.Join(", ", student.Exams)}");
+        }
+
+        foreach (var student in loadedStudentsA)
+        {
+            Console.WriteLine($"After deserialization - {student.FirstName} {student.LastName}: Average Score - {student.AverageScore}");
+        }
+    }
+}
+
+public class Student
+{
+    private string _firstNameValue;
+    private string _lastNameValue;
+    private int[] _exams;
+    private double _averageScore;
+
+    public Student(string firstName, string lastName, int[] exams)
+    {
+        _firstNameValue = firstName;
+        _lastNameValue = lastName;
+        _exams = exams;
+        _averageScore = CalculateAverageScore();
+    }
+
+    private double CalculateAverageScore()
+    {
+        if (_exams == null || _exams.Length == 0)
+         {
+            return 0.0;
+        }
+
+        int sum = 0;
+        foreach (int exam in _exams)
+        {
+            sum += exam;
+        }
+        return (double)sum / _exams.Length;
+    }
+
+    public string FirstName
+    {
+        get { return _firstNameValue; }
+    }
+
+    public string LastName
+    {
+        get { return _lastNameValue; }
+    }
+
+    public int[] Exams
+    {
+        get { return _exams; }
+    }
+
+    public double AverageScore
+    {
+        get { return _averageScore; }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Student: {_lastNameValue} {_firstNameValue}, Average Score: {_averageScore}");
+    }
+}
